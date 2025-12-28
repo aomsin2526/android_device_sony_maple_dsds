6c706ee5352ecc6fda86a7aa7274c7ba5a8b620a
Chattrapat Sangmanee <aomsin27@hotmail.co.th>
2025-12-29 04:39:47 +0700

    WITH_ADB_INSECURE=true

--- lineage_maple_dsds.mk
+++ lineage_maple_dsds.mk
@@ -18,6 +18,8 @@ TARGET_BOOTANIMATION_HALF_RES := true
 ### LINEAGE
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+WITH_ADB_INSECURE := true
+
 ## Device identifier. This must come after all inclusions
 PRODUCT_NAME := lineage_maple_dsds
 PRODUCT_DEVICE := maple_dsds
